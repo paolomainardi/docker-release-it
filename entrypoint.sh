9e8f182f9511a4e3cb36ceeda763eb49f5db81f4
Paolo Mainardi <paolomainardi@gmail.com>
2023-10-31 12:50:19 +0100

    fix: quote variables

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,10 @@ fi
 # Reconfigure the PATH to have nodejs binaries installed.
 export PATH=/release-it/node_modules/.bin:${PATH}
 
-if [[ ! -z $GITLAB_USER_NAME ]]; then
+if [[ ! -z "${GITLAB_USER_NAME}" ]]; then
   git config --global user.name "${GITLAB_USER_NAME}"
 fi
-if [[ ! -z $GITLAB_USER_EMAIL ]]; then
+if [[ ! -z "${GITLAB_USER_EMAIL}" ]]; then
   git config --global user.email "${GITLAB_USER_EMAIL}"
 fi
 
@@ -24,13 +24,13 @@ fi
 git config --global --add safe.directory "${BASE}"
 
 # Set branch on gitlab-ci environment.
-if [[ ! -z $CI_COMMIT_REF_NAME ]]; then
+if [[ ! -z "${CI_COMMIT_REF_NAME}" ]]; then
   git checkout "${CI_COMMIT_REF_NAME}"
 fi
 
 # If we set a gitlab project token, we can use it to push to the repository.
 # Taken from here: https://github.com/sparkfabrik/spark-k8s-deployer/blob/master/templates/scripts/ci_releases/setup_repo_for_writing.sh
-if [[ ( ! -z ${GITLAB_PROJECT_RW_AND_API_TOKEN} ) ]]; then
+if [[ ! -z "${GITLAB_PROJECT_RW_AND_API_TOKEN}" ]]; then
   NEWREMOTEURL=$(echo "${CI_REPOSITORY_URL}" | sed -e "s|.*@\(.*\)|$CI_SERVER_PROTOCOL://$GITLAB_PROJECT_RW_AND_API_TOKEN@\1|")
   git remote set-url origin "${NEWREMOTEURL}"
 fi
